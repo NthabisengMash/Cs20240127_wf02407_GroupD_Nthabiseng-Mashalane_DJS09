3bac9400d74ef9904366795552611f62b892ec8a
Nthabiseng Mashalane <nthabimashalane0@gmail.com>
2024-11-24 22:30:16 +0200

    Committed changes

---
+++ dist/utils.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.showReviewTotal = showReviewTotal;
+exports.populateUser = populateUser;
+exports.showDetails = showDetails;
+exports.makeMultiple = makeMultiple;
+exports.getTopTwoReviews = getTopTwoReviews;
+const reviewTotalDisplay = document.querySelector('#reviews');
+const returningUserDisplay = document.querySelector('#returning-user');
+const userNameDisplay = document.querySelector('#user');
+const enums_1 = require("../src/enums");
+function showReviewTotal(value, reviewer, isLoyalty) {
+    const iconDisplay = enums_1.LoyaltyUser.GOLD_USER ? '⭐' : '';
+    reviewTotalDisplay.innerHTML = value.toString() + ' review' + makeMultiple(value) + ' | last reviewed by ' + reviewer + ' ' + iconDisplay;
+}
+function populateUser(isReturning, userName) {
+    if (isReturning) {
+        returningUserDisplay.innerHTML = 'back';
+    }
+    userNameDisplay.innerHTML = userName;
+}
+function showDetails(value, element, price) {
+    if (value) {
+        const priceDisplay = document.createElement('div');
+        priceDisplay.innerHTML = price.toString() + '/night';
+        element.appendChild(priceDisplay);
+    }
+}
+function makeMultiple(value) {
+    if (value > 1 || value == 0) {
+        return 's';
+    }
+    else
+        return '';
+}
+function getTopTwoReviews(reviews) {
+    const sortedReviews = reviews.sort((a, b) => b.stars - a.stars);
+    return sortedReviews.slice(0, 2);
+}
+//# sourceMappingURL=utils.js.map
