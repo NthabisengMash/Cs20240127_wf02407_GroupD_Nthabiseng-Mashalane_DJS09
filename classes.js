f7109aad4befd383d30ac6ac2df1e6a22611828f
Nthabiseng Mashalane <nthabimashalane0@gmail.com>
2024-11-24 21:43:08 +0200

    commit changes

---
+++ classes.js
@@ -0,0 +1,12 @@
+"use strict";
+// Classes
+Object.defineProperty(exports, "__esModule", { value: true });
+class MainProperty {
+    constructor(src, title, reviews) {
+        this.src = src;
+        this.title = title;
+        this.reviews = reviews;
+    }
+}
+exports.default = MainProperty;
+//# sourceMappingURL=classes.js.map
